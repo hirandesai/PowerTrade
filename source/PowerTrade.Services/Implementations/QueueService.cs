d0b83ae6bf8c1a8da1a4d6411f7899c4394f7890
Hiren Desai <desaihiren2009@gmail.com>
2025-03-15 10:10:50 +0100

    Added queue services

---
+++ source/PowerTrade.Services/Implementations/QueueService.cs
@@ -0,0 +1,18 @@
+﻿using PowerTrade.Services.Abstracts;
+
+namespace PowerTrade.Services.Implementations
+{
+    public class QueueService<T> : IQueueService<T>
+    {
+        public async Task AddAsync(T item)
+        {
+            await Task.CompletedTask;
+        }
+
+        public async Task<T> Read()
+        {
+            await Task.CompletedTask;
+            return default(T);
+        }
+    }
+}
