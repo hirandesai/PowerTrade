402d6af1844d1ce9c67c5118e2895b5fa28af221
Hiren Desai <desaihiren2009@gmail.com>
2025-03-15 09:35:29 +0100

    Added logging

---
+++ source/PowerTrade/Infrastructure/Configurations/LoggingConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace PowerTrade.Infrastructure.Configurations
+{
+    public static class LoggingConfiguration
+    {
+        public static void AddLogging(IServiceCollection serviceProvider)
+        {
+            Log.Logger = new LoggerConfiguration()
+                            .WriteTo.Console()
+                            .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+                            .CreateLogger();
+
+            serviceProvider.AddLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.AddSerilog();
+            });
+        }
+    }
+}
--- source/PowerTrade/Program.cs
+++ source/PowerTrade/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+#if DEBUG
+Environment.SetEnvironmentVariable("DOTNET_ENVIRONMENT", "Development");
+#endif
+
+var env = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "Production";
+var builder = Host.CreateDefaultBuilder(args)                    
+                    .ConfigureAppConfiguration(config =>
+                    {
+                        config
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env}.json", true, true)
+                        .AddEnvironmentVariables();
+
+                    })
+                    .ConfigureServices((hostContext, services) =>
+                    {
+                        services.AddLogging();
+                    })
+                    .UseConsoleLifetime()
+                    .Build();
+
+var logger = builder.Services.GetService<ILogger<Program>>();
+logger.LogInformation($"{nameof(PowerTrade)} starting");
+
+await builder.RunAsync();
+
